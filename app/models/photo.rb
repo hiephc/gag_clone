c495e9a899cfce5f862bc106805887ee3414d95a
hiephc <falleninterface@gmail.com>
2017-04-18 09:08:12 +0700

    limit upload file size

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -6,6 +6,6 @@ class Photo < ApplicationRecord
   validates :title, presence: true
   private
   def image_size_validation
-    errors[:image] << "should be less than 500KB" if image.size > 0.5.megabytes
+    errors[:image] << "should be less than 5MB" if image.size > 5.megabytes
   end
 end
