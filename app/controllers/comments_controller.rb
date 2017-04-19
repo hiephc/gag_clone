0928bde4760dce7962e2bf5889ecefd5fd601b52
hiephc <falleninterface@gmail.com>
2017-04-19 12:03:21 +0700

    Add authentication for comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  before_filter :authenticate_user!
   before_action :find_commentable
 
   def new
@@ -18,7 +19,7 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:body)
+    params.require(:comment).permit(:body).merge(user_id: current_user.id)
   end
 
   def find_commentable
--- db/schema.rb
+++ db/schema.rb
@@ -16,10 +16,10 @@ ActiveRecord::Schema.define(version: 20170418100018) do
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.string   "title"
     t.text     "body"
     t.integer  "commentable_id"
     t.string   "commentable_type"
+    t.integer  "user_id"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
     t.index ["commentable_id", "commentable_type"], name: "index_comments_on_commentable_id_and_commentable_type", using: :btree
