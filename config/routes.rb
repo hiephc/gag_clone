99c7b1bd94f931dc8e0d8948161e335d32dbb9f9
hiephc <falleninterface@gmail.com>
2017-04-19 09:49:06 +0700

    add comment function

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :photos
+  resources :photos do
+  resources :comments
+  end
   devise_for :users, controllers: { confirmations: 'confirmations' }
   root 'homepage#index'
-
+  resources :comments do
+    resources :comments
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
